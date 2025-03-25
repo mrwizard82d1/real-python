77b3fe6446cae3d394694043fe3dd1d845a79209
Larry Jones <mrwizard82d1@gmail.com>
2025-03-25 15:40:05 -0500

    A maxsize=0 produces a very large queue at start

--- concurrency-async/threading-in-python/13-queue.py
+++ concurrency-async/threading-in-python/13-queue.py
@@ -1,13 +1,6 @@
 """
 Using the Python `queue` module and `threading.Event` to
 improve our producer consumer queue.
-
-Remember, debugging multithreaded code is very error-prone.
-The presenter encountered an error that I avoided, but the
-error presenting as a deadlock was unexpected (except that,
-I believe, a thread was encountering an exception and
-"eating it" resulting in an overall error with no message
-indicating the problem).
 """
 import concurrent.futures
 import queue
@@ -16,13 +9,10 @@ import threading
 import time
 
 
-FINISH = 'THE END'
-
-
 class Pipeline(queue.Queue):
     def __init__(self):
         # `queue.Queue` handles locking "automagically"
-        super().__init__(maxsize=10)
+        super().__init__(maxsize=0)
 
     def get_message(self):
         message = self.get()
@@ -46,6 +36,8 @@ def consumer(pipeline, is_finished):
     while not pipeline.empty() or not is_finished.is_set():
         print(f'Queue size is {pipeline.qsize()}')
         message = pipeline.get_message()
+        # Because the consumer sleeps, the producer puts many messages
+        # on the queue before the consumer begins to consume messages
         time.sleep(random.random())
 
 
