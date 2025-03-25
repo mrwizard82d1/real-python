852063f55e4ad9a0082e77056397bd408b4eb12c
Larry Jones <mrwizard82d1@gmail.com>
2025-03-25 11:21:57 -0500

    Two threads: main thread and function

--- concurrency-async/threading-in-python/thread-03.py
+++ concurrency-async/threading-in-python/thread-03.py
@@ -2,16 +2,18 @@
 Demonstrate two threads.
 """
 
+import threading
 import time
 
 
 def my_func(name):
-    print(f'my_func() called with {name}')
+    print(f'my_func() called with "{name}"')
     time.sleep(10)
-    print(f'my_func() called with {name} ended')
+    print('my_func() ended')
     return True
 
 if __name__ == '__main__':
-    print('main() started')
-    my_func('realpython')
-    print('main() ended')
+    print('main() started on main thread')
+    t = threading.Thread(target=my_func, name='my_func', args=['realpython'])
+    t.start()
+    print('main() ended on main thread')
