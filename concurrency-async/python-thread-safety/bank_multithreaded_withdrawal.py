f4f709df8442aeb642b0e08cdf7a7c04e8d903ab
Larry Jones <mrwizard82d1@gmail.com>
2025-03-26 12:21:08 -0500

    An example of a race condition

---
+++ concurrency-async/python-thread-safety/bank_multithreaded_withdrawal.py
@@ -0,0 +1,25 @@
+import time # For `sleep()`
+from concurrent.futures import ThreadPoolExecutor
+
+
+class BankAccount:
+    def __init__(self, balance=0):
+       self.balance = balance
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            new_balance = self.balance - amount
+            time.sleep(0.1) # Simulate a delay
+            self.balance = new_balance
+        else:
+            raise ValueError('Insufficient funds')
+
+
+account = BankAccount(1000)
+
+
+with ThreadPoolExecutor(max_workers=2) as executor:
+    executor.submit(account.withdraw, 500)
+    executor.submit(account.withdraw, 700)
+
+print(f'Final balance: {account.balance}')
