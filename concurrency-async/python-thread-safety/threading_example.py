31909b144fba22f3726e7fba5e835933c753eb34
Larry Jones <mrwizard82d1@gmail.com>
2025-03-26 12:12:50 -0500

    Running code concurrently

---
+++ concurrency-async/python-thread-safety/threading_example.py
@@ -0,0 +1,20 @@
+"""
+Creation of four threads.
+
+From Real Python tutorial, "Python Thread Safety: Using a Lock and Other Techniques."
+"""
+
+import threading
+import time
+from concurrent.futures import ThreadPoolExecutor
+
+
+def threaded_function():
+    for number in range(3):
+        print(f'Printing from {threading.current_thread().name}. {number=}')
+        time.sleep(0.1)
+
+
+with ThreadPoolExecutor(max_workers=4, thread_name_prefix="Worker") as executor:
+    for _ in range(4):
+        executor.submit(threaded_function)
