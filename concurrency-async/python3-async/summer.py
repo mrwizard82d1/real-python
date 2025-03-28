ddb943d3c050edc837d32a7e2cad322b5fbd2c04
Larry Jones <mrwizard82d1@gmail.com>
2025-03-28 18:53:15 -0500

    Query a single block of random numbers
    The video uses the URL,
    'https://qrng.anu.edu.au.API/jsonI.php?length={n}&type=uint16'. This URL
    has a limit of **one** request per minute. In practice, this limit is
    far too slow for my work.
    
    Instead, I used the URL
    'https://www.random.org/integers/?num={n}&min=1&max=100000&col=1&base=10&format=plain&rnd=new'.
    This endpoint seems adequate for my work.

--- concurrency-async/python3-async/summer.py
+++ concurrency-async/python3-async/summer.py
@@ -9,8 +9,27 @@ import time
 import aiohttp
 
 
+async def worker(name, n, session):
+    """
+    A worker function that collects n items
+    :param name: The name of the session
+    :param n: The number of items to request
+    :param session: The HTTP session to use
+    :return:
+    """
+    print(f'worker {name} started')
+    url = f'https://www.random.org/integers/?num={n}&min=1&max=100000&col=1&base=10&format=plain&rnd=new'
+    response = await session.request(method='GET', url=url)
+    value = await response.text()
+    return value
+
 async def main():
-    await asyncio.sleep(1)
+    """
+    Create session and run requests asynchronously.
+    """
+    async with aiohttp.ClientSession() as session:
+        response = await worker(name='Bob', n=3, session=session)
+        print(f'{response=}, type={type(response)}')
 
 
 if __name__ == '__main__':
