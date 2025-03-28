8675a8bf85d529c4e6d5bb2837434275f1490056
Larry Jones <mrwizard82d1@gmail.com>
2025-03-28 15:11:05 -0500

    Produce odd values using a generator

---
+++ concurrency-async/python3-async/theory.py
@@ -0,0 +1,21 @@
+def odds(start, stop):
+    """
+    Produces a sequence of odd numbers between start and stop, inclusive.
+
+    :param start: The first value of the sequence
+    :param stop: The last value of the sequence
+    :return: A generated sequence
+    """
+
+    for odd in range(start, stop + 1, 2):
+        # `yield` will return a value and "pause>
+        yield odd
+
+
+def main():
+    odd_values = [odd for odd in odds(3, 15)]
+    print(odd_values)
+
+
+if __name__ == '__main__':
+    main()
