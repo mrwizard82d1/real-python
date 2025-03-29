6cf776f2de11f6e5f3e403cbc8300368f7284f69
Larry Jones <mrwizard82d1@gmail.com>
2025-03-29 15:05:28 -0500

    Using multiprocessor.Pool to distribute computing

---
+++ concurrency-async/python-parallel-processing/fibonacci_multiprocessing_pool.py
@@ -0,0 +1,16 @@
+"""
+Create a pool of processes to exercise data parallelism.
+"""
+
+import multiprocessing
+
+
+def fib(n):
+    return n if n < 2 else fib(n - 1) + fib(n - 2)
+
+
+if __name__ == '__main__':
+    with multiprocessing.Pool(processes=4) as pool:
+        results = pool.map(fib, range(40))
+        for i, result in enumerate(results):
+            print(f'fib({i})={result}')
