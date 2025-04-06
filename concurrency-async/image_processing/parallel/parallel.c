baa21f35dcfe57780afabdc3177dcac87cf5aa34
Larry Jones <mrwizard82d1@gmail.com>
2025-04-05 19:24:05 -0500

    Pre-calculated lookup table to optimize calc

--- concurrency-async/image_processing/parallel/parallel.c
+++ concurrency-async/image_processing/parallel/parallel.c
@@ -5,13 +5,21 @@
 void process(
     unsigned char* pixels,
     int length,
+    int offset,
     float ev,
     float gamma
 ) {
-    // Starting of `offset` and the step size of 3 allows each worker
-    // thread to work independently of the other (two) threads.
+    // Lookup table to optimize calculation
+    unsigned char lookup_table[256];
+
+    // Fill lookup table
     for (int i = offset; i < length; i += 3) {
         float value = powf((pixels[i] = 255.0f * ev), gamma) * 255.0f;
-        pixels[i] = (unsigned char) fmin(fmax(0.0f, value), 255.0f);
+        lookup_table[i] = (unsigned char) fmin(fmax(0.0f, value), 255.0f);
+    }
+
+    // Use lookup table to calculate new pixel values
+    for (int i = offset; i < length; i += 3) {
+        pixels[i] = lookup_table[pixels[i]];
     }
 }
