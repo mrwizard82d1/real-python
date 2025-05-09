25f3e360af77f7b2875db8042d61bed78a842570
Larry Jones <mrwizard82d1@gmail.com>
2025-05-09 12:50:22 -0500

    Demonstrate logging json to a file

---
+++ loguru-simpler-logging/loguru_json_file.py
@@ -0,0 +1,11 @@
+from loguru import logger
+
+logger.remove()
+logger.add(
+    "app.json",
+    serialize=True,
+    format="{time} | {level} | {message}",
+)
+
+logger.info("Application started")
+logger.warning("Memory usage is high")
