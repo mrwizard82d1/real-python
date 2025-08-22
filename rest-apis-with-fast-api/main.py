e2f2adad9ee752de96d910575dccbfb7b2e35ebe
Larry Jones <mrwizard82d1@gmail.com>
2025-08-22 12:43:01 -0500

    GET shapes and shapes/{shape_id}
    However, getting a specific shape by id return `null`.
    
    This unexpected result is because we have **not** correctly typed our
    input data and Pydantic assumes that untyped parameters are of type
    `str` (not `int` as we think). Consequently no item has id "2"
    eventually resulting in a `null` value.

--- rest-apis-with-fast-api/main.py
+++ rest-apis-with-fast-api/main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
 
+
+shapes = [
+    {"item_name": "Circle", "no_of_sides": 1, "id": 1},
+    {"item_name": "Triangle", "no_of_sides": 3, "id": 2},
+    {"item_name": "Octagon", "no_of_sides": 8, "id": 3},
+]
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/shapes")
+async def get_shapes():
+    return shapes
+
+
+@app.get("/shapes/{shape_id}")
+async def get_shape_by_id(shape_id):
+    for shape in shapes:
+        if shape["id"] == shape_id:
+            return shape
