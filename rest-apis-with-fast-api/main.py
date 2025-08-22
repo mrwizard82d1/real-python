9409d836b803d3bfde9f35086f291c199377d6e0
Larry Jones <mrwizard82d1@gmail.com>
2025-08-22 14:44:22 -0500

    Handle errors finding shapes

--- rest-apis-with-fast-api/main.py
+++ rest-apis-with-fast-api/main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException
 
 shapes = [
     {"item_name": "Circle", "no_of_sides": 1, "id": 1},
@@ -25,3 +24,5 @@ async def get_shape_by_id(shape_id: int) -> dict[str, str | int] | None:
     for shape in shapes:
         if shape["id"] == shape_id:
             return shape
+
+    raise HTTPException(status_code=404, detail=f"Shape with id, {shape_id}, not found.")
