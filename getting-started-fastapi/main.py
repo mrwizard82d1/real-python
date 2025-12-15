1add46cd605ee38ebc95b36a3f564978e5c3125b
Larry Jones <mrwizard82d1@gmail.com>
2025-12-15 12:42:34 -0600

    Two books in repository; get "/books"

--- getting-started-fastapi/main.py
+++ getting-started-fastapi/main.py
@@ -1,9 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
-@app.get("/")
-def home():
-    return {"message": "Hello, FastAPI!"}
+books = [
+    {"id": 1,
+     "title": "Python Basics",
+     "author": "Real P.",
+     "pages": 635},
+    {"id": 2,
+     "title": "Breaking the Rules",
+     "author": "Stephen G.",
+     "pages": 99}
+]
+
+
+class Book(BaseModel):
+    title: str
+    author: str
+    pages: int
+
+
+@app.get("/books")
+def get_books(limit: int | None = None):
+    """Get all books, optionally limited by `limit`."""
+    if limit:
+        return {"books": books[:limit]}
+
+    return {"books" : books}
