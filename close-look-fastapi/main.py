4b13c89b4f41c2444ae8a4a62d10882aecf9f097
Larry Jones <mrwizard82d1@gmail.com>
2025-12-17 16:48:51 -0600

    Add query parameters

--- close-look-fastapi/main.py
+++ close-look-fastapi/main.py
@@ -1,6 +1,7 @@
 import random
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
 
 
 app = FastAPI()
@@ -10,9 +11,36 @@ app = FastAPI()
 def read_main():
     return {'message': 'Welcome to the Randomizer API'}
 
+# Declare path and query parameters with types
 @app.get('/random/{max_value}')
 def get_random_number(max_value: int):
     return {
         'max': max_value,
         'random_number': random.randint(1, max_value),
+    }
+
+# Add query parameters
+@app.get('/random-between')
+def get_random_number_between(
+        min_value: Annotated[int, Query(
+            title='Minimum Value',
+            description='The minimum random number',
+            ge=1,
+            le=1000
+        )] = 1,
+        max_value: Annotated[int, Query(
+            title="Maximum Value",
+            description='The maximum random number',
+            ge=1,
+            le=1000
+        )] = 99,
+):
+    if min_value > max_value:
+        raise HTTPException(status_code=400,
+                            detail="min_value cannot be greater that max_value")
+
+    return {
+        'min': min_value,
+        'max': max_value,
+        'random_number': random.randint(min_value, max_value)
     }
--- close-look-fastapi/test_main.py
+++ close-look-fastapi/test_main.py
@@ -41,3 +41,55 @@ def test_read_parameters_error():
     assert detail['loc'] == ['path', 'max_value']
     assert detail['msg'].startswith('Input should be a valid integer')
     assert detail['input'] == 'ten'
+
+
+def test_no_query_parameters():
+    response = client.get(f'/random-between')
+    assert response.status_code == 200
+    json_response = response.json()
+    assert 'min' in json_response
+    assert json_response['min'] == 1
+    assert 'max' in json_response
+    assert json_response['max'] == 99
+    assert 'random_number' in json_response.keys()
+    assert 1 <= json_response['random_number'] <= 99
+
+
+def test_min_only_query_parameters():
+    min_value = 38
+    response = client.get(f'/random-between?min_value={min_value}')
+    assert response.status_code == 200
+    json_response = response.json()
+    assert 'min' in json_response
+    assert json_response['min'] == min_value
+    assert 'max' in json_response
+    assert json_response['max'] == 99
+    assert 'random_number' in json_response.keys()
+    assert min_value <= json_response['random_number'] <= 99
+
+
+def test_max_only_query_parameters():
+    max_value = 75
+    response = client.get(f'/random-between?max_value={max_value}')
+    assert response.status_code == 200
+    json_response = response.json()
+    assert 'min' in json_response
+    assert json_response['min'] == 1
+    assert 'max' in json_response
+    assert json_response['max'] == max_value
+    assert 'random_number' in json_response.keys()
+    assert 1 <= json_response['random_number'] <= max_value
+
+
+def test_all_query_parameters():
+    min_value = 580
+    max_value = 673
+    response = client.get(f'/random-between?min_value={min_value}&max_value={max_value}')
+    assert response.status_code == 200
+    json_response = response.json()
+    assert 'min' in json_response
+    assert json_response['min'] == min_value
+    assert 'max' in json_response
+    assert json_response['max'] == max_value
+    assert 'random_number' in json_response.keys()
+    assert min_value <= json_response['random_number'] <= max_value
