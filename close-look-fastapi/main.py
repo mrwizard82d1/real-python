10cf90ffaf1d6352245effd5686f80bc2d8ca848
Larry Jones <mrwizard82d1@gmail.com>
2026-02-10 16:02:17 -0600

    The root api, `/` maps to the home function

--- close-look-fastapi/main.py
+++ close-look-fastapi/main.py
@@ -4,10 +4,5 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+async def home():
+    return {"message": "Welcome to the Randomizer API"}
