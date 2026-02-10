965aed1dec73e2bd2e44fbbc1c271abc8dc40a4b
Larry Jones <mrwizard82d1@gmail.com>
2026-02-10 16:07:28 -0600

    Add endpoint for '/random/{max_value}'
    Generates a random number between 1 and max_value (inclusive).

--- close-look-fastapi/main.py
+++ close-look-fastapi/main.py
@@ -1,3 +1,5 @@
+import random
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +8,11 @@ app = FastAPI()
 @app.get("/")
 async def home():
     return {"message": "Welcome to the Randomizer API"}
+
+@app.get("/random/{max_value}")
+def get_random_number(max_value: int):
+    """Generate a random number between 1 and max_value (inclusive)."""
+    return {
+        "max": max_value,
+        "random_number": random.randint(1, max_value)
+    }
