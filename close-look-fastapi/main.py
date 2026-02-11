d43909add366f6821634a98309462889a99d1dc2
Larry Jones <mrwizard82d1@gmail.com>
2026-02-11 10:03:51 -0600

    FastAPI using annotated query parameters

--- close-look-fastapi/main.py
+++ close-look-fastapi/main.py
@@ -1,13 +1,42 @@
 import random
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
 
 app = FastAPI()
+
+
+@app.get("/random_between")
+def get_random_number_between(
+        min_value: Annotated[int, Query(
+            title="Minimum value",
+            description="The minimum random number",
+            ge=1,
+            le=1000
+        )] = 1,
+        max_value: Annotated[int, Query(
+            title="Maximum value",
+            description="The maximum random number",
+            ge=1,
+            le=1000
+        )] = 99
+):
+    if min_value > max_value:
+        raise HTTPException(status_code=400,
+                            detail=f"Minimum value, {min_value}, "
+                                   f"cannot be greater than maximum value, {max_value}.")
+
+    return {
+        "min": min_value,
+        "max": max_value,
+        "random_number": random.randint(min_value, max_value),
+    }
 
 
 @app.get("/")
 async def home():
     return {"message": "Welcome to the Randomizer API"}
+
 
 @app.get("/random/{max_value}")
 def get_random_number(max_value: int):
